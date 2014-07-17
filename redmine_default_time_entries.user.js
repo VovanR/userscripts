ce9e4c6aaf2d7dadf8ad4cc6baf3b636121e8275
VovanR <mail@vovanr.com>
2014-07-17 14:53:03 +0400

    +Redmine Default Time Entries

---
+++ redmine_default_time_entries.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Redmine Default Time Entries
+// @description Sets default settings in new time entries
+// @namespace   https://github.com/VovanR
+// @include     http://*/redmine/*/time_entries/new*
+// @version     1.0.0
+// @author      VovanR (https://github.com/VovanR)
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+
+(function () {
+
+	setTimeout(function () {
+		$('#time_entry_activity_id').val(9);
+	}, 500);
+
+})();

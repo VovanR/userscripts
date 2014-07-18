72e230d17474a50055516e488c0fb049667b826a
VovanR <mail@vovanr.com>
2014-07-18 12:57:46 +0400

    +Redmine Default Time Entries

---
+++ redmine_default_tt_activity.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Redmine Default Time Tracker Activity
+// @description Sets default time tracker activity
+// @namespace   https://github.com/VovanR
+// @include     http://*/redmine/*
+// @version     1.0.0
+// @author      VovanR (https://github.com/VovanR)
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+
+(function () {
+
+	$('#main').on('click', '.tt_stop', function () {
+		setTimeout(function () {
+			$('#tt_activity').val(9).trigger('change');
+		}, 1000);
+	});
+
+})();

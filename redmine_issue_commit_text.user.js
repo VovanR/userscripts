ba62b9129fd433e33ef7f98acb861a607cf98f21
VovanR <mail@vovanr.com>
2015-01-13 13:56:45 +0300

    +Redmine Issue Commit Text

---
+++ redmine_issue_commit_text.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Redmine Issue Commit Text
+// @description For fastest copy-paste to git commit
+// @namespace   https://github.com/VovanR
+// @include     http://*/redmine/issues/*
+// @version     1.0.0
+// @author      VovanR (https://github.com/VovanR)
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+
+(function (global) {
+
+	var $content = $('#content');
+	var $issueTitle = $content.find('h2').first();
+	var $issueName = $content.find('.subject').find('h3');
+
+	var issueNumber = $issueTitle.text();
+	issueNumber = issueNumber.substr(issueNumber.indexOf('#'));
+
+	var commitText = 'Issue ' + issueNumber + ': ' + $issueName.text();
+
+	var $commitInput = $('<input type=\'text\' readonly value=\'' + commitText + '\' style=\'width: 100%;\' >')
+	$content.find('.issue.details').prepend($commitInput);
+
+})(window);
